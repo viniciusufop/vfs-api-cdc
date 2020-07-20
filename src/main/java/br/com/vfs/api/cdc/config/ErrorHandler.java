41157c37aaa5f0ce2ad5a5319152a21982933e52
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-07-20 14:13:26 -0300

    melhorando estrutura do handle de erros

--- src/main/java/br/com/vfs/api/cdc/config/ErrorHandler.java
+++ src/main/java/br/com/vfs/api/cdc/config/ErrorHandler.java
@@ -1,16 +1,17 @@
 package br.com.vfs.api.cdc.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -19,10 +20,21 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        var errors = ex.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
+        var errors = getErrors(ex);
         log.info("M=handleMethodArgumentNotValid, errors:{}", errors);
         var url = ((ServletWebRequest) request).getRequest().getRequestURI();
         var errorMessage = new ErrorMessage(errors, url);
         return super.handleExceptionInternal(ex, errorMessage, headers, status, request);
+    }
+
+    private List<String> getErrors(final MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(this::getFinalMessage)
+                .collect(Collectors.toList());
+    }
+
+    private String getFinalMessage(final FieldError fieldError) {
+        return String.format("field: %s, message: %s",fieldError.getField(), fieldError.getDefaultMessage());
     }
 }
